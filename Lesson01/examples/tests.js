e98b902b6b907389539e57298247d1406b44efdc
Fernanda Passos <fernanda@midiacom.uff.br>
2024-09-13 09:56:12 +0100

    Examples

--- Lesson01/examples/tests.js
+++ Lesson01/examples/tests.js
@@ -4,43 +4,32 @@ let a = 10000000000000000 + 1;
 let y = 123;
 let z = 123n;
 console.log("1:", x, y, z, a);
-console.log("2:", typeof(x), typeof(y), typeof(z), typeof(a));
+console.log("2:", typeof x, typeof y, typeof z , typeof a);
 console.log("3:", x == y, x == z, x === y, x === z);
 
-// Object
-let obj = {x: 3, y: 2};
-console.log("4:", typeof obj);
-
 // Scope
-console.log("5:", h);
-//console.log("5.1:", g);
+console.log("4:", h);
+//console.log("4.1:", g);
 { 
     var h = 5;
     let g = "g";
 }
-console.log("6:", h);
-//console.log("6.1:", g);
+console.log("5:", h);
+//console.log("5.1:", g);
 
 // Function and scope
-console.log("7:", test());
-console.log("8:", typeof test);
-//console.log("8.1:", k);
+console.log("6:", test());
+console.log("7:", typeof test);
+//console.log("7.1:", k);
 
 function test(){
     var k = 4;
-    console.log("9: test with k =", k);
+    console.log("8: test with k =", k);
 }
 
 // Arrow function
-sum = (x, y) => x + y;
-console.log("10:", sum(2, 4));
-
-// Array
-const arr = [1, 2]
-console.log("11:", typeof arr);
-
-// Json
-console.log("12:", JSON.stringify({a: 2, b: [1, 2]}));
+const sum = (x, y) => x + y;
+console.log("9:", sum(2, 4));
 
 // Closure
 function setCounter() {
@@ -51,11 +40,12 @@ function setCounter() {
 }
 
 const counter = setCounter();
+console.log(typeof counter);
 console.log(counter());
 console.log(counter());
 console.log(counter());
 
-/*
+
 // Stack call:
 function chicken() {
     return egg();
@@ -64,4 +54,3 @@ function egg() {
     return chicken();
 }
 console.log(chicken() + " came first.");
-*/
