997f509e958d576a2c0cda6e1305cdaec99e36c8
Fernanda Passos <fernanda@midiacom.uff.br>
2024-09-24 17:58:05 +0100

    Examples

--- Lesson03/examples/example.mjs
+++ Lesson03/examples/example.mjs
@@ -3,3 +3,11 @@ import {dayName} from "./dayname.mjs";
 let now = new Date();
 
 console.log(`Today is ${dayName(now.getDay())}`);
+
+function a(x, y){
+    return {a: x, b: y};
+}
+
+console.log(a(1, 2))
+const f = (x, y) => ({a: x, b: y});
+console.log(f(1, 2))
