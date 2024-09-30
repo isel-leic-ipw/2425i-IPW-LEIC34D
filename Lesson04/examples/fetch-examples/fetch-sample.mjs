249338dcd911368b3d5fde9be96309d177dcdbfa
Fernanda Passos <fernanda@midiacom.uff.br>
2024-09-30 11:01:44 +0100

    Examples

--- Lesson04/examples/fetch-examples/fetch-sample.mjs
+++ Lesson04/examples/fetch-examples/fetch-sample.mjs
@@ -1,5 +1,4 @@
 const URL_CHUCK_NORRIS_JOKE = "https://api.chucknorris.io/jokes/mLUozC5_T3uidSRnZ0nNgQ";
-const URL_RANDOM_JOKE = "https://official-joke-api.appspot.com/random_joke";
 
 
 // const responseP = fetch(URL_CHUCK_NORRIS_JOKE);
@@ -15,10 +14,3 @@ fetch(URL_CHUCK_NORRIS_JOKE)
     .then(text => text.length)
     .then(len => console.log(len))
     .catch((err) => console.log("Error"));
-
-
-// fetch(URL_RANDOM_JOKE)
-//     .then(resp => resp.json())
-//     .then(obj => {console.log(obj.setup); return obj.punchline;})
-//     .then(text => setTimeout(() => {console.log(text);}, 3000))
-//     .catch(err => {console.log("Error")});
