39459c17f384eef8ed2a3483476c022b94a6e799
Fernanda Passos <fernanda@midiacom.uff.br>
2024-10-07 15:31:49 +0100

    Examples

--- Lesson04/examples/fetch-examples/fetch-title-webpage.js
+++ Lesson04/examples/fetch-examples/fetch-title-webpage.js
@@ -1,10 +1,10 @@
-const URL = "https://eloquentjavascript.net/11_async.html";
+const URL = "https://loquentjavascript.net/11_async.html";
 
 // Using promises explicitly
 fetch(URL)
     .then((resp) => resp.text())
     .then((text) => console.log(getTitle(text)))
-    .catch((err) => console.log("error", err));
+    .catch((err) => console.error("Error!!"));
 
 function getTitle(text){
     let start = text.indexOf("<title>");
@@ -14,10 +14,15 @@ function getTitle(text){
 
 // Using async/await
 async function showTitleWebPage(url) {
-    const rsp = await fetch(url);                // Response
-    const text = await rsp.text();               // String  
-    const title = getTitle(text);                // String
-    console.log(title);                          // undefined  
+    try {
+        const rsp = await fetch(url);                // Response
+        const text = await rsp.text();               // String  
+        const title = getTitle(text);                // String
+        console.log(title);                          // undefined  
+    }
+    catch (e){
+        console.error("Error!!", e);
+    }
 }
 
 showTitleWebPage(URL);
