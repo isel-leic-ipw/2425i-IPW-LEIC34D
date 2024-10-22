653d2f55ca855065b5757aae7e8e32a1e60e2e3a
Fernanda Passos <fernanda@midiacom.uff.br>
2024-10-22 11:46:38 +0100

    Example: hello world with Express module.

---
+++ Lesson05/example-hello-express/hello-world-express.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const PORT = 8000;  // Port number for the tests
+const app = express(); // Express function returns an app
+
+// Single endpoint to URI "/"
+app.get("/", sendHello);
+
+// App listening...
+app.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`),
+);
+
+
+// FUNCTIONS:
+
+function sendHello(req, res){
+    console.log(req.headers);
+    res.send("Hello World2!");
+}
+
+// Testing: go to the browser and access the URL: http://localhost:8000/
+// Or use the Rest Client in VScode or Postman to make the following request:
+//      GET http://localhost:8000/ HTTP/1.1
