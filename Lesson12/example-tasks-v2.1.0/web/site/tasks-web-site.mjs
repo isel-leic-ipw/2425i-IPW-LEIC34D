81d00f2523a316ef175f4e1dd2ae26bb5f3e5a50
Fernanda Passos <fernanda@midiacom.uff.br>
2024-12-19 13:32:30 +0000

    Web Task application with client-side javascript to DELETE and PUT with fetch to the Web API.

--- Lesson12/example-tasks-v2.1.0/web/site/tasks-web-site.mjs
+++ Lesson12/example-tasks-v2.1.0/web/site/tasks-web-site.mjs
@@ -1,8 +1,7 @@
 import { errors } from "../../commons/errors.mjs";
-//import * as tasksServices from "../../services/tasks-services.mjs";
 import errorToHttp from '../errors-to-http-responses.mjs';
 
-// FUNCTIONS (WEB API):
+// FUNCTIONS (WEB SITE):
 
 export default function init(tasksServices){
 
@@ -31,7 +30,7 @@ export default function init(tasksServices){
 
   function ensureToken(req, res, next){
     const token = getToken(req);
-    console.log("Token:", token);
+    //console.log("Token:", token);
     if (token){
       req.userToken = token;
       next();
@@ -48,10 +47,9 @@ export default function init(tasksServices){
 
   function getResponseError(res, err){
     const responseError = errorToHttp(err);
-    console.log(responseError.body);
+    //console.log(responseError.body);
     res.status(responseError.status);
     return res.render("errors-view", responseError.body);
-    //return res.json(responseError.body);
   }
 
   function local_getAllTasks(req, res){
@@ -62,7 +60,7 @@ export default function init(tasksServices){
   function local_getTask(req, res){
     const taskId = req.params.taskId;
     const taskPromise = tasksServices.getTask(taskId, req.userToken);
-    return taskPromise.then(task => res.render("task-view", task));
+    return taskPromise.then(task => res.render("task-view", {task, token: req.userToken}));
   }
 
   function local_addTask(req, res){
@@ -75,7 +73,6 @@ export default function init(tasksServices){
 
   function local_deleteTask(req, res){
     const taskId = req.params.taskId;
-    console.log(taskId, req.userToken);
     const deleteTaskPromise = tasksServices.deleteTask(taskId, req.userToken);
     return deleteTaskPromise.then(deleteTask => res.redirect("/site/tasks"));
   }
@@ -90,7 +87,7 @@ export default function init(tasksServices){
 
   // Auxiliary module function
   function getToken(req) {
-    // TODO: add Web site authentication (after, in the last classes)
+    // TODO: add Web site authentication (with passport module, req brings token in the session)
     //return "86130b25-a37e-4f19-9842-549b2fd5bb2c"; // asilva in Elastic DB
     return "b0506867-77c3-4142-9437-1f627deebd67"; // asilva in Memory
   }
